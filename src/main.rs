7b552ed0775827176dafb5a98c2d7fc1b31fd839
Kacper Kafara <kacperkafara@gmail.com>
2024-01-31 23:36:20 +0100

    chore: compute some hashes & add dependency on clap (#1)
    * chore: Compute some first hashes
    
    * chore: add dependency on clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use md5;
+use std::fs;
+use std::path;
+
+
 fn main() {
-    println!("Hello, world!");
+    let content = include_str!("../data/input01.txt");
+
+    println!("{content}");
+    let digest = md5::compute(content.as_bytes());
+    println!("{digest:?}");
+
+    let Ok(binary_content) = fs::read(path::Path::new("data/input01.txt")) else {
+        return;
+    };
+
+    println!("{binary_content:?}");
+    let binary_digest = md5::compute(binary_content);
+    println!("{binary_digest:?}");
 }
